91a3a33522f3fa60a4b40204ba0b7372acf93da1
Adrian <assassinr3d@gmail.com>
2018-07-16 19:13:35 -0400

    Initial commit

---
+++ setup.bat
@@ -0,0 +1,6 @@
+echo "# FirstTest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/assassinr3d/FirstTest.git
+git push -u origin master
